7e2a13c9bbdeb5df3e8e2719046fbe036371b046
Qin <166709128+SorryQin@users.noreply.github.com>
2026-04-27 17:07:41 +0800

    Create train-5t.sh

---
+++ train-5t.sh
@@ -0,0 +1,26 @@
+ACCELERATE_USE_CPU=0 \
+CUDA_VISIBLE_DEVICES=0 \
+WORLD_SIZE=1 \
+RANK=0 \
+MASTER_PORT=29500 \
+nohup python train_pisa_per_step_5t.py --train_data_dir ./temp_data_on_the_fly/ \
+  --train_data_dir ./temp_data_on_the_fly/ \
+  --pretrained_model_name_or_path ./models/sdxl-base \
+  --train_batch_size 2 \
+  --output_dir ./output/run5t \
+  --max_train_steps 70000 \
+  --gradient_accumulation_steps 1 \
+  --enable_xformers_memory_efficient_attention \
+  --mixed_precision no \
+  --learning_rate 3e-5 \
+  --lr_scheduler cosine_with_restarts \
+  --lr_num_cycles 2 \
+  --lr_power 0.5 \
+  --lr_warmup_steps 20 \
+  --resolution 512 \
+  --checkpointing_steps 10000 \
+  --gan_loss_type multilevel_sigmoid \
+  --lambda_gan 1.0 \
+  --gan_step 40000 \
+  --lpips --edge --lambda_lpips 2 \
+  --rank 8
